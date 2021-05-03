9278fec998c122c8a2fedb39b9516deff7602392
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-05-03 09:37:58 -0400

    Add user, blog, comment models

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const User = require("./User");
+const Comment = require("./Comment");
+const Blog = require("./Blog");
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Blog, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Blog.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Comment, Blog };
