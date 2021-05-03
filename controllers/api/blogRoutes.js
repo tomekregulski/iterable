8f53f4804184f25d1a28f50d17874d07a1b33dec
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-05-03 09:55:07 -0400

    Add user, blog routes

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const withAuth = require("../../utils/auth");
+const { Blog } = require("../../models");
+// router.get('/', async (req, res) => {
+//   try {
+//     const projects = res.status(200).json(newProject);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newBlog = await Blog.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newBlog);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const blogData = await Blog.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogData) {
+      res.status(404).json({ message: "No project found with this id!" });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
