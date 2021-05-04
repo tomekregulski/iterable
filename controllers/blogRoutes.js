418c859ba028ec1fa18f3388417ff9609a653582
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-05-04 09:48:23 -0400

    add edit post route

--- controllers/blogRoutes.js
+++ controllers/blogRoutes.js
@@ -71,4 +71,34 @@ router.get("/admin/:id", withAuth, async (req, res) => {
   }
 });
 
+router.get("/admin/edit/:id", withAuth, async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: "blog_author",
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          as: "blog_comments",
+          include: {
+            model: User,
+            as: "comment_author",
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
+
+    const editSingleBlogData = blogData.get({ plain: true });
+    // res.render("blog-admin");
+    res.render("edit-blog", { editSingleBlogData });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
